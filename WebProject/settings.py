d6b7db57d94f93c3f829cf8387e552409b04f0f8
tayyab-balti <tayyabkazmi555@gmail.com>
2024-08-15 18:33:31 +0500

    Initial commit - pushing Django project to GitHub

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -127,3 +125,9 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'segmentation_app/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
